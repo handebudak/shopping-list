4f8ab6e38187a4c41e97ef189916a3aca580df1a
Hande Budak <bdkhande@gmail.com>
2024-03-26 14:44:21 +0300

    mobile shopping list

---
+++ index.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL : "https://realtime-database-e17c0-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", () => {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+
+    inputFieldEl.value = ''
+    
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+})
